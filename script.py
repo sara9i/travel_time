4a215df66aa89e2976b74b5390933c675f0d9402
sara iftikhar <sara.iftikharsi@gmail.com>
2023-01-22 02:08:51 +0400

    refactor

--- script.py
+++ script.py
@@ -51,34 +51,7 @@ def calculate_travel_time_with_sort(deliveries):
 
     # Sort the deliveries based on start time
     deliveries.sort(key=lambda x: x["start_time"])
-    # Create a dictionary to store the current delivery times for each delivery person
-    current_deliveries = {}
-    for delivery in deliveries:
-        start_time = delivery["start_time"]
-        end_time = delivery["end_time"]
-        delivery_person = delivery["delivery_person"]
-
-        # Check if the delivery person is currently on another delivery
-        if delivery_person in current_deliveries:
-            # Calculate the overlap in time
-            previous_delivery = current_deliveries[delivery_person]
-            # ignore if the last delivery's time is less than current delivery's start time i.e there's no overlap
-            if previous_delivery["end_time"] > start_time:
-                overlap = (
-                    min(end_time, previous_delivery["end_time"])
-                    - max(start_time, previous_delivery["start_time"])
-                ).total_seconds() / 60
-                # Subtract the overlap from the total travel time
-                travel_times[delivery_person] -= overlap
-
-        # Add the new delivery time to the current deliveries dictionary
-        current_deliveries[delivery_person] = {
-            "start_time": start_time,
-            "end_time": end_time,
-        }
-        # Add the total travel time for the delivery
-        travel_times[delivery_person] += (end_time - start_time).total_seconds() / 60
-    return dict(travel_times)
+    return calculate_travel_time(deliveries)
 
 
 with open("delivery_logs.csv", "r") as f:
